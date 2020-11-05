01a5ef36af4b5063985d2c62c60f548b02a9f84a
yair pinhas <yp6969@gmail.com>
2020-11-05 18:19:13 +0200

    lab3-c

---
+++ lab3/lab3_c.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def swap(a,b):
+    temp=a
+    a=b
+    b=temp
+
+img = plt.imread('picture.jpg')
+
+img2 = np.array(img)
+
+print(img2)
+for i in range(len(img)):
+    for j in range(len(img[0])):
+        r,g,b = img2[i,j]
+        img2[i,j] = b,g,r
+
+
+
+# plt.figure()
+# plt.subplot(1,2,1)
+plt.imshow(img2)
+plt.imsave('image.png',img2)
+plt.show()
+
