eea2a3e3a7153dfb0a64a5ffeab69e4b2a8ea952
Yair Pinhas <ypinhas@mellanox.com>
2021-01-05 00:37:11 +0200

    dasdasd

--- HW5/game_of_life.py
+++ HW5/game_of_life.py
@@ -1,47 +1,95 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-def neighbors(A,i,j):
-    size = len(A)
-    sum_neighbors = sum([A[(i+1) % size , k % size] for k in range(j-1, j+2)])
-    sum_neighbors += sum([A[(i-1) % size , k % size] for k in range(j-1, j+2)])
-    sum_neighbors += A[i,(j-1) % size]
-    sum_neighbors += A[i,(j+1) % size]
-    return sum_neighbors
-
-
 def mat_neigbors_update(A):
-    return np.array([[neighbors(A,i,j) for j in range(len(A[0]))] for i in range(len(A))])
+    up = A[-1: , :]
+    up = np.append(up , A[:-1,:] , axis=0)
+
+    down = A[1:,:]
+    down = np.append(down , A[:1,:] , axis=0)
+
+    right = A[:,-1:]
+    right = np.append(right,  A[:, :-1] , axis=1)
+
+    left = A[:,1:]
+    left = np.append(left,  A[:, :1] , axis=1)
+
+    u_left = up[:,1:]
+    u_left = np.append(u_left, up[:, :1], axis=1)
+
+    u_right = up[:, -1:]
+    u_right = np.append(u_right, up[:, :-1], axis=1)
+
+    d_left = down[:, 1:]
+    d_left = np.append(d_left, down[:, :1], axis=1)
+
+    d_right = down[:, -1:]
+    d_right = np.append(d_right, down[:, :-1], axis=1)
+
+    return (up + down + left + right + u_left + u_right + d_left + d_right)
 
 
-def evolution(A,B):
+
+
+def evolution(A,B, min , max):
     for i in range(len(A)):
         for j in range(len(A)):
-            if B[i,j] == 3 : A[i,j] = 1
-            elif B[i,j] > 3 or B[i,j] < 2: A[i,j] = 0
+            if B[i,j] == max : A[i,j] = 1
+            elif B[i,j] > max or B[i,j] < min: A[i,j] = 0
     return A
 
+def glider(A):
+    A[1, 1] = 1
+    A[2, 0] = 1
+    A[3, 0:3] = 1
 
-def game_of_life(min = 0 , max = 8 , dens_0 = 20):
-    size = 100
+def Pulsar(A):
+
+    X = np.zeros((17, 17))
+    X[2, 4:7] = 1
+    X[4:7, 7] = 1
+    X += X.T
+    X += X[:, ::-1]
+    X += X[::-1, :]
+    A[20:37 , 20:37] = X
+
+
+def Unbounded (A):
+    unbounded = [[1, 1, 1, 0, 1],
+                 [1, 0, 0, 0, 0],
+                 [0, 0, 0, 1, 1],
+                 [0, 1, 1, 0, 1],
+                 [1, 0, 1, 0, 1]]
+    X = np.zeros((30, 40))
+    X[15:20, 18:23] = unbounded
+    A[100:130 , 100:140] = X
+
+
+def game_of_life(min = 2 , max = 3 , dens_0 = 66):
+    size = 256
     Live_mat = np.zeros((size,size))
 
-    Live_mat[1 , 1] = 1
-    Live_mat[2 , 0] = 1
-    Live_mat[3 , 0:3] = 1
+# initiate
+    Unbounded(Live_mat)
+    glider(Live_mat)
+    Pulsar(Live_mat)
+
+
 
     Neighbors_mat = mat_neigbors_update(Live_mat)
     dens = dens_0
+
     while(dens > 0 ):
-
+        plt.clf()
         plt.imshow(Live_mat , cmap='hot')
-        plt.pause(0.1)
-
-        Live_mat = evolution(Live_mat,Neighbors_mat)
+        plt.colorbar()
+        plt.show(block=False)
+        plt.pause(0.01)
+        Live_mat = evolution(Live_mat,Neighbors_mat , min, max)
         Neighbors_mat = mat_neigbors_update(Live_mat)
-    plt.show()
+        dens = sum(sum(Live_mat))
 
+    plt.show()
 
 
 
